15265df0c177b54cab433f6e8e3f004c545c9da0
Yuchen Huang <YuchenHuang97@Gmail.com>
2023-04-04 00:02:53 -0400

    field of view

---
+++ Starwar/Assets/Scripts/AI/Fighter_AI/CheckEnemyInFOV.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using BehaviorTree;
+
+public class CheckEnemyInFOV : Node
+{
+    public Transform self;
+    public float viewAngle, viewRange;
+    public CheckEnemyInFOV(Transform transform, float viewAngle, float viewRange)
+    {
+        this.self = transform;
+        this.viewAngle = viewAngle;
+        this.viewRange = viewRange;
+    }
+}
---
+++ Starwar/Assets/Scripts/AI/Fighter_AI/FighterBT.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BehaviorTree;
+
+public class FighterBT : Tree
+{
+
+    protected override Node SetupTree()
+    {
+        Node root = new Selector(new List<Node>
+        {
+            new Sequence(new List<Node>
+            {
+               // new CheckEnemyInAttackRange(this.transform),
+                //new TaskAttack(this.transform)
+            }),
+            //TODO :Add attack behavior
+            new Sequence(new List<Node>
+            {
+                //new CheckEnemyInFOVRange(transform),
+               // new TaskGoToTarget(transform),
+               
+            }),
+       // new TaskPatrol(transform, waypoints)
+        }); ;
+
+        return root;
+    }
+}
