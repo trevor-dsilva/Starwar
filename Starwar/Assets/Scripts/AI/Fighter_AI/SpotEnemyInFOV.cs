7d8d89be8f5f7644ec1a261368725c0cfad1ed4e
Yuchen Huang <YuchenHuang97@Gmail.com>
2023-04-04 03:53:26 -0400

    Spot Enemy

---
+++ Starwar/Assets/Scripts/AI/Fighter_AI/SpotEnemyInFOV.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using BehaviorTree;
+
+public class SpotEnemyInFOV : Node
+{
+    public Transform self;
+    public float viewRange;
+    public ShipManager.ShipBelong shipBelong;
+    // Will cast a ray toward enemy ship, ray should be hit obstacles and enemy ship, but not ally ship.
+    public LayerMask viewMask;
+    public SpotEnemyInFOV(Transform transform, float viewRange, ShipManager.ShipBelong shipBelong, LayerMask viewMask)
+    {
+        this.self = transform;
+        this.viewRange = viewRange;
+        this.shipBelong = shipBelong;
+        this.viewMask = viewMask;
+    }
+
+    public override NodeState Evaluate()
+    {
+        foreach (Ship ship in ShipManager.Ships(shipBelong))
+        {
+            if (ship.IsSpotted) { continue; }
+            Vector3 direction = ship.transform.position - self.position;
+            if (Physics.Raycast(self.position, direction, out RaycastHit hitInfo, viewRange, viewMask))
+            {
+                if (hitInfo.collider.gameObject == ship.gameObject)
+                {
+                    ship.Spotted();
+                }
+            }
+        }
+        return NodeState.SUCCESS;
+    }
+}
